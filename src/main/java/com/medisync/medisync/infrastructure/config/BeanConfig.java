ac38bbdc6e1eaf84bb0b8ca4b5fc395441fb0ba3
Andres <andres111420@gmail.com>
2026-04-22 15:39:15 -0500

    feat(application): unificacion de bean

---
+++ src/main/java/com/medisync/medisync/infrastructure/config/BeanConfig.java
@@ -0,0 +1,68 @@
+package com.medisync.medisync.infrastructure.config;
+
+import com.medisync.medisync.application.usecases.gestor.*;
+import com.medisync.medisync.application.usecases.inventario.*;
+import com.medisync.medisync.application.usecases.medicamento.*;
+import com.medisync.medisync.domain.repositories.*;
+import com.medisync.medisync.domain.services.IPasswordEncoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+
+    // Medicamento
+    @Bean
+    public CrearMedicamentoUseCase crearMedicamentoUseCase(IMedicamentoRepository medicamentoRepository) {
+        return new CrearMedicamentoUseCase(medicamentoRepository);
+    }
+
+    @Bean
+    public EliminarMedicamentoUseCase eliminarMedicamentoUseCase(IMedicamentoRepository medicamentoRepository) {
+        return new EliminarMedicamentoUseCase(medicamentoRepository);
+    }
+
+    @Bean
+    public ObtenerMedicamentosUseCase obtenerMedicamentosUseCase(IMedicamentoRepository medicamentoRepository) {
+        return new ObtenerMedicamentosUseCase(medicamentoRepository);
+    }
+
+    @Bean
+    public ObtenerMedicamentoPorIdUseCase obtenerMedicamentoPorIdUseCase(IMedicamentoRepository medicamentoRepository) {
+        return new ObtenerMedicamentoPorIdUseCase(medicamentoRepository);
+    }
+
+    @Bean
+    public ActualizarMedicamentoUseCase actualizarMedicamentoUseCase(IMedicamentoRepository medicamentoRepository) {
+        return new ActualizarMedicamentoUseCase(medicamentoRepository);
+    }
+
+    // Inventario
+    @Bean
+    public CrearInventarioUseCase crearInventarioUseCase(IInventarioRepository inventarioRepository) {
+        return new CrearInventarioUseCase(inventarioRepository);
+    }
+
+    @Bean
+    public ObtenerInventariosUseCase obtenerInventariosUseCase(IInventarioRepository inventarioRepository) {
+        return new ObtenerInventariosUseCase(inventarioRepository);
+    }
+
+    // Gestor
+    @Bean
+    public CrearGestorUseCase crearGestorUseCase(IGestorRepository gestorRepository,
+                                                 IPasswordEncoder passwordEncoder,
+                                                 CrearInventarioUseCase crearInventarioUseCase) {
+        return new CrearGestorUseCase(gestorRepository, passwordEncoder, crearInventarioUseCase);
+    }
+
+    @Bean
+    public ObtenerGestoresUseCase obtenerGestoresUseCase(IGestorRepository gestorRepository) {
+        return new ObtenerGestoresUseCase(gestorRepository);
+    }
+
+    @Bean
+    public ObtenerGestorPorIdUseCase obtenerGestorPorIdUseCase(IGestorRepository gestorRepository) {
+        return new ObtenerGestorPorIdUseCase(gestorRepository);
+    }
+}
